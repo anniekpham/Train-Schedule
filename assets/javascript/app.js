ea80329a55a55bf3988eed26fbb6b5c1869ce06c
anniekpham <anniekpham@yahoo.com>
2019-03-29 01:43:14 -0700

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBj6fj9PkkibdkZoxsShjOibmKNPI7xgJU",
+    authDomain: "train-schedule-b5bb0.firebaseapp.com",
+    databaseURL: "https://train-schedule-b5bb0.firebaseio.com",
+    projectId: "train-schedule-b5bb0",
+    storageBucket: "",
+    messagingSenderId: "431470405365"
+  }
+  
+  firebase.initializeApp(config);
