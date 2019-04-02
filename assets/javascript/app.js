d1d865c2b4bc4e9cbcaed35ed5ed427dcb7c89f0
anniekpham <anniekpham@yahoo.com>
2019-04-02 00:40:21 -0700

    finished schedule

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,42 +9,48 @@ const config = {
 
 firebase.initializeApp(config);
 
-let db = firebase.firestore(),
-    id = db.collection('TrainSchedule').doc().id
+let db = firebase.firestore()
 
 const init = () => {
     document.querySelector('.addtrain').style.visibility = 'hidden'
-    document.querySelector('.addbtn').addEventListener('click', e => {
+    document.querySelector('.addbtn').addEventListener('click', () => {
         document.querySelector('.addtrain').style.visibility = ''
     })
 }
 
 document.querySelector('#submit').addEventListener('click', e => {
     e.preventDefault()
+    let duration = document.querySelector('#duration').value,
+        firsttrain = document.querySelector('#first-train').value,
+        time = moment(`${firsttrain}`,'HH:mm').valueOf(),
+        durInUnix = 60000 * parseInt(duration),
+        ETA = time += durInUnix,
+        minaway = (ETA - moment())/60000
     if (document.querySelector('#duration').value > 0){
+        let id = db.collection('TrainSchedule').doc().id
         db.collection('TrainSchedule').doc(id).set({
             destination: document.querySelector('#destination').value,
-            firsttrain: document.querySelector('#first-train').value,
+            ETA: ETA,
             duration: document.querySelector('#duration').value,
+            minaway: minaway
         })
         document.querySelector('#destination').value = ''
         document.querySelector('#first-train').value = ''
-        document.querySelector('#duration').value = ''
+        document.querySelector('#duration').value = ''   
     }
 })
 
 db.collection('TrainSchedule').onSnapshot(({docs}) =>{
     document.querySelector('.traininfo').innerHTML = ''
     docs.forEach(doc => {
-        let {destination, firsttrain, duration} = doc.data()
+        let {destination, ETA, duration, minaway} = doc.data()
         let traininfo = document.createElement('div')
         traininfo.innerHTML = `
         <div class="col s4 info">${destination}</div>
         <div class="col s3 info">${duration}</div>
-        <div class="col s3 info">${firsttrain}</div>
-        <div class="col s2 info">${duration}</div>
+        <div class="col s3 info">${moment(ETA, 'x').format('hh:mm a')}</div>
+        <div class="col s2 info">${Math.ceil(minaway)}</div>
         `
-
         document.querySelector('.traininfo').append(traininfo)
     })
 })
